efe2a46414b123c16462f100f035b793381a62ba
silveur <silvere.letellier@gmail.com>
2022-03-23 15:01:59 +0100

    Fetching all products

--- packages/appibase/src/next.config.cjs
+++ packages/appibase/src/next.config.cjs
@@ -3,6 +3,6 @@ const commerce = require('./commerce.config.json')
 module.exports = {
   commerce,
   images: {
-    domains: ['localhost', 'shopify.vercel.store', 'vercel.store'],
+    domains: ['localhost', 'shopify.vercel.store', 'vercel.store', 'demo.vercel.store'],
   },
 }
